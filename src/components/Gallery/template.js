a9a0b445a75f630a05054977cd8a09546087ce45
fshenton <fshenton@gmail.com>
2019-08-09 13:56:02 +0100

    prev and next buttons now contain svg elements instead of using background-image. Also added some transitions to them.

--- src/components/Gallery/template.js
+++ src/components/Gallery/template.js
@@ -13,8 +13,18 @@ template.innerHTML = `
 			<output class="${s.outputWrapper}" for="prevButton nextButton thumbnail1 thumbnail2 thumbnail3 thumbnail4">
 				<img id="mainImage" class="${s.outputImage}"/>
 			</output>
-			<button id="prevButton" class="${s.navButton} ${s.prevButton}"></button>
-			<button id="nextButton" class="${s.navButton} ${s.nextButton}"></button>
+			<button id="prevButton" class="${s.navButton} ${s.prevButton}">
+				<svg class="${s.prevSVG} ${s.svg}" viewBox="0 0 129 129">
+					<path d="M34.6,67.4c-1.6-1.6-1.6-4.2,0-5.8L88.5,7.7c1.6-1.6,4.2-1.6,5.8,0s1.6,4.2,0,5.8l-51,51l51,51c1.6,1.6,1.6,4.2,0,5.8
+					c-0.8,0.8-1.8,1.2-2.9,1.2c-1.1,0-2.1-0.4-2.9-1.2L34.6,67.4z"/>
+				</svg
+			</button>
+			<button id="nextButton" class="${s.navButton} ${s.nextButton}">
+				<svg class="${s.nextSVG} ${s.svg}" viewBox="0 0 129 129">
+					<path d="m40.4,121.3c-0.8,0.8-1.8,1.2-2.9,1.2s-2.1-0.4-2.9-1.2c-1.6-1.6-1.6-4.2 0-5.8l51-51-51-51c-1.6-1.6-1.6-4.2 0-5.8 
+					1.6-1.6 4.2-1.6 5.8,0l53.9,53.9c1.6,1.6 1.6,4.2 0,5.8l-53.9,53.9z"/>
+				</svg
+			</button>
 			<fieldset>
 				<div class="${s.thumbnails}">
 					<input id="thumbnail1" class="${s.thumbnail}" name="thumbnail" data-num="1" type="radio"/>
